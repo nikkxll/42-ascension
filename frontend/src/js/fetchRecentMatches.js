1718b0e0be5b9db9c009ea49d64120d4949f3b44
MCP <91376232+madfcat@users.noreply.github.com>
2024-11-26 00:53:49 +0200

    Update Recent Matches logic with custom event

--- frontend/src/js/fetchRecentMatches.js
+++ frontend/src/js/fetchRecentMatches.js
@@ -1,53 +1,69 @@
 console.log("Fetch recent matches script loaded");
-window.addEventListener('load', fetchRecentMatches);
+document.addEventListener("DOMContentLoaded", () => {
+	fetchRecentMatches();
+
+	window.updateRecentMatchesEvent = new CustomEvent("updateRecentMatches", {
+		detail: {
+			message: "Updating the recent matches",
+			timestamp: Date.now(),
+		},
+	});
+
+	document.addEventListener("updateRecentMatches", async (event) => {
+		console.log("Message:", event.detail.message);
+		// console.log("Timestamp:", event.detail.timestamp);
+		await fetchRecentMatches()
+
+	});
+});
 
 async function fetchRecentMatches() {
-	try{
-		const response = await fetch('/api/matches/', {
-			method: 'GET',
+	try {
+		const response = await fetch("/api/matches/", {
+			method: "GET",
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
 		});
 		if (!response.ok) {
 			throw new Error(`HTTP error! status: ${response.status}`);
 		}
-	
+
 		const { data } = await response.json();
 		if (!data) {
-			throw new Error('No data');
+			throw new Error("No data");
 		}
 
-
-		const matchList = document.querySelector('.match-results');
+		const matchList = document.querySelector(".match-results");
 		data?.matches?.map((match) => {
 			const newDiv = document.createElement("div");
-			console.log(match);
 			let scoreArr;
 			let isWinner1 = undefined;
 			if (match.score !== null) {
-				scoreArr = match.score.split(":");
+				scoreArr = match.score.split(":").map((str) => (Number(str)));
 				isWinner1 = scoreArr[0] > scoreArr[1];
+				console.log(scoreArr[0], scoreArr[1], isWinner1);
 			}
-			console.log(match.players[0]?.displayName, match.players[0]?.username)
-			console.log(match.players[1]?.displayName, match.players[1]?.username)
 			newDiv.innerHTML = `
 				<li class="match-result">
 				<p onclick="goToMatchView()" class="match-link">
 					<span class=${isWinner1 === true ? "winner-name" : ""}>
-						${match.players[0]?.displayName ?? match.players[0]?.username }
+						${match.players[0]?.displayName ?? match.players[0]?.username}
 					</span> vs <span class=${isWinner1 === false ? "winner-name" : ""}>
 						${match.players[1]?.displayName ?? match.players[1]?.username}
 					</span> ${match.score ? "[" + match.score + "]" : "[0:0]"}
 				</p>
-				</li>
-			`
+				</li>`;
 			matchList.appendChild(newDiv);
 		});
-
-		// <span class="winner-name">${match.players[0].displayName || match.players[0].user.username }</span> vs ${match.players[1].username || match.players[1].user.username} [${match.score}]
-	}
-	catch (error) {
+	} catch (error) {
 		console.error(error);
 	}
+}
+
+// Update recent matches from DB
+function updateRecentMatches() {
+	const matchList = document.querySelector(".match-results");
+	matchList.innerHTML = "";
+	document.dispatchEvent(window.updateRecentMatchesEvent);
 }
