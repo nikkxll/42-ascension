e2ef4edfc73a10001dcef670c80891c19a1e6b7f
MCP <91376232+madfcat@users.noreply.github.com>
2024-12-07 17:18:47 +0200

    Fixed score highlighting, Add teams to recent matches
    Co-authored-by: Dmitrii Nikiforov <dmitr.nikiforov@gmail.com>
    Co-authored-by: MCP <91376232+madfcat@users.noreply.github.com>

--- frontend/src/js/recentMatches.js
+++ frontend/src/js/recentMatches.js
@@ -1,11 +1,11 @@
 console.log("Fetch recent matches script loaded");
 document.addEventListener("DOMContentLoaded", async () => {
-	window.state.recentMatches = await fetchRecentMatches();
-	renderRecentMatches();
+	await renderRecentMatches();
 });
 
-function renderRecentMatches() {
-	const data = window.state.recentMatches
+async function renderRecentMatches() {
+	window.state.recentMatches = await fetchRecentMatches();
+	const data = window.state.recentMatches;
 	console.log(data);
 	const matchList = document.querySelector(".match-results");
 	matchList.innerHTML = "";
@@ -16,19 +16,50 @@ function renderRecentMatches() {
 	data?.matches?.map((match) => {
 		const newDiv = document.createElement("div");
 		let isWinner1 = undefined;
-		if (match.score !== null) {
-			isWinner1 = match.score[0] > match.score[1];
+		if (match.score !== null) {	
+			isWinner1 = Number(match.score[0]) > Number(match.score[1]);
 		}
 		const matchScore = match.score ? match.score[0] + ":" + match.score[1] : "0:0";
+
+		let team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
+							${match.players[0]?.displayName ?? match.players[0]?.username}
+						</div>`;
+		let team2Div = `<div class=${isWinner1 === false ? "winner-name" : ""}>
+							${match.players[1]?.displayName ?? match.players[1]?.username}
+						</div>`;
+		if (match.players.length == 4) {
+			// console.log(match.score[0], match.score[1], isWinner1);
+			team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
+							<div class="player-small-text">
+							${match.players[0]?.displayName ?? match.players[0]?.username}
+							</div>
+							<div class="player-small-text">
+							${match.players[1]?.displayName ?? match.players[1]?.username}
+							</div>
+						</div>`;
+			team2Div = `<div class=${isWinner1 === false ? "winner-name" : ""}>
+							<div class="player-small-text">
+							${match.players[2]?.displayName ?? match.players[2]?.username}
+							</div>
+							<div class="player-small-text">
+							${match.players[3]?.displayName ?? match.players[3]?.username}
+							</div>
+						</div>`;
+		}
+
+		const matchScoreDiv = `<div>
+					${match.score ? "[" + matchScore + "]" : "[0:0]"}
+				</div>`;
+
+
 		newDiv.innerHTML = `
 			<li class="match-result">
-			<p onclick="goToMatchView()" class="match-link">
-				<span class=${isWinner1 === true ? "winner-name" : ""}>
-					${match.players[0]?.displayName ?? match.players[0]?.username}
-				</span> vs <span class=${isWinner1 === false ? "winner-name" : ""}>
-					${match.players[1]?.displayName ?? match.players[1]?.username}
-				</span> ${match.score ? "[" + matchScore + "]" : "[0:0]"}
-			</p>
+			<div onclick="goToMatchView()" class="match-link">
+				${team1Div}
+				<div class="versus-text">vs</div>
+				${team2Div}
+				${matchScoreDiv}
+			</div>
 			</li>`;
 		matchList.appendChild(newDiv);
 	});
