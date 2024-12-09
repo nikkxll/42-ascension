f83d19fd58c590c8f8d4de3f50bf81bf542383da
Dmitrii Nikiforov <dmitr.nikiforov@gmail.com>
2024-12-09 23:44:29 +0200

    continue working on recent tournaments

--- frontend/src/js/renderTournamentLobby.js
+++ frontend/src/js/renderTournamentLobby.js
@@ -112,7 +112,7 @@ function createTournament() {
   function generateSemifinals(filledPlayers) {
     shuffle(filledPlayers);
     const [player1, player2, player3, player4] = filledPlayers;
-    
+
     window.tournamentState.userIds = [];
     window.tournamentState.userIds.push(
       player1.id,
@@ -120,15 +120,15 @@ function createTournament() {
       player3.id,
       player4.id
     );
-    
+
     firstSemifinalContent.innerHTML = "";
     secondSemifinalContent.innerHTML = "";
-    
+
     firstSemifinalContent.innerHTML = `
     <h1 class="tournament-match-title">Semifinal 1</h1>
     ${generateMatchContent(player1, player2)}
     `;
-    
+
     secondSemifinalContent.innerHTML = `
     <h1 class="tournament-match-title" >Semifinal 2</h1>
     ${generateMatchContent(player3, player4)}
@@ -168,8 +168,12 @@ function createTournament() {
 
 function loadTournament(tournamentId) {
   const tournaments = window.state.tournaments.tournaments;
-  const tournament = tournaments.find(t => t.id === tournamentId);
-  const tournamentTitle = tournament.name;
+  const tournament = tournaments.find((t) => t.id === tournamentId);
+
+  console.log('Tournament IDs:', tournaments.map(t => t.id));
+  console.log('Tournament ID to find:', tournamentId);
+  console.log(tournaments);
+  console.log(tournament);
 
   const tournamentGrid = document.querySelector(".tournament-grid");
   const firstSemifinalContent = document.getElementById(
@@ -178,170 +182,172 @@ function loadTournament(tournamentId) {
   const secondSemifinalContent = document.getElementById(
     "secondSemifinalContent"
   );
+  const tournamentTitle = document.getElementById("tournamentTitle");
 
-  console.log(tournament);
+  const neededPlayers = getNeededPlayers();
+  const tournamentPlayers = getTournamentPlayers();
 
-  const players = [
-    ...tournament.matches[0]?.players.map(player => player?.displayName),
-    ...tournament.matches[1]?.players.map(player => player?.displayName)
-  ];
-  
-  console.log(players);
-  // console.log(tournament.matches[0]?.players[0]?.displayName);
-  // console.log(tournament.matches[0]?.players[1]?.displayName);
+  ifallLoggedIn(neededPlayers);
 
+  function getNeededPlayers() {
+    const players = [];
 
-  // function getPlayers() {
-  //   const players = window.state.loggedInUsers;
-  //   if (players.length < 3) {
-  //     console.error(
-  //       "Error: At least three users must be logged in to start the tournament."
-  //     );
-  //     alert(
-  //       "Error: At least three users must be logged in to start the tournament."
-  //     );
-  //     goToLobby();
-  //     return [];
-  //   }
-  //   return players;
-  // }
+    const sf1 = tournament?.matches?.[0];
+    const sf2 = tournament?.matches?.[1];
+    const f = tournament?.winner;
 
-  // function getTournamentName() {
-  //   const tournamentNameInput = document.getElementById("tournamentName");
-  //   return tournamentNameInput.value.trim();
-  // }
+    if (sf1?.score && sf2?.score && !f) {
+      players.push(sf1.players?.[0]?.id, sf2.players?.[1]?.id);
+    } else if (sf1?.score && !f) {
+      players.push(
+        sf1.score[0] > sf1.score[1]
+          ? sf1.players?.[0]?.id
+          : sf1.players?.[1]?.id,
+        ...(sf2?.players?.map((player) => player?.id) || [])
+      );
+    } else {
+      players.push(
+        ...(sf1?.players?.map((player) => player?.id) || []),
+        ...(sf2?.players?.map((player) => player?.id) || [])
+      );
+    }
+    return players;
+  }
 
-  // function generatePlayerCards(players) {
-  //   tournamentGrid.innerHTML = "";
+  function getTournamentPlayers() {
+    const players = [];
 
-  //   const filledPlayers = [...players];
-  //   if (filledPlayers.length < 4) {
-  //     filledPlayers.push({
-  //       id: 1,
-  //       username: `AI`,
-  //       winRate: "75%",
-  //       avatar: "./assets/ai_profile.png",
-  //     });
-  //   }
+    const sf1 = tournament?.matches?.[0];
+    const sf2 = tournament?.matches?.[1];
+    players.push(...(sf1?.players || []), ...(sf2?.players || []));
 
-  //   filledPlayers.forEach((player, index) => {
-  //     const playerCard = document.createElement("div");
-  //     playerCard.className = "game-player-card";
-  //     playerCard.innerHTML = `
-  //         <article class="tournament-game-player-card-inner">
-  //           <h2 class="game-player-number">Player ${index + 1}</h2>
-  //           <img
-  //             loading="lazy"
-  //             src="${player.avatar || "./assets/default_avatar.png"}"
-  //             alt="Player avatar"
-  //             class="game-player-avatar"
-  //           />
-  //           <h3 class="game-player-name">${player.username}</h3>
-  //           <div class="game-player-win-rate-container">
-  //             <p class="game-player-statistics-param">Win rate</p>
-  //             <p class="game-player-statistics-param-number last">${
-  //               player.winRate ?? 0
-  //             }</p>
-  //           </div>
-  //         </article>
-  //       `;
-  //     tournamentGrid.appendChild(playerCard);
-  //   });
-  //   return filledPlayers;
-  // }
+    return players;
+  }
 
-  // function addLabelsToPlayers(players) {
-  //   players.forEach((player, index) => {
-  //     player.label = `Player ${index + 1}`;
-  //   });
-  // }
+  function ifallLoggedIn(neededPlayers) {
+    const loggedInPlayers = window.state.loggedInUsers;
+    const loggedInPlayersIds = loggedInPlayers.map((player) => player.id);
 
-  // function shuffle(array) {
-  //   for (let i = array.length - 1; i > 0; i--) {
-  //     const randomIndex = Math.floor(Math.random() * (i + 1));
-  //     [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
-  //   }
-  // }
+    if (!arraysChecker(loggedInPlayersIds, neededPlayers)) {
+      console.error(
+        "Error: Not enough players are logged in to finish the tournament"
+      );
+      alert("Error: Not enough players are logged in to finish the tournament");
+      goToLobby();
+    }
+  }
 
-  // function generateMatchContent(player1, player2) {
-  //   return `
-  //       <div class="tournament-match-players">
-  //         <div class="tournament-match-player-info-left">
-  //           <img loading="lazy" src="${
-  //             player1.avatar || "./assets/default_avatar.png"
-  //           }" alt="Player avatar" class="tournament-match-avatar" />
-  //           <h3 class="game-player-name tournament">${player1.label}</h3>
-  //         </div>
-  //         <h2 class="tournament-match-left-score">-</h2>
-  //         <img class="tournament-match-vs-logo" src="./assets/vs_logo.png" alt="Versus logo" />
-  //         <h2 class="tournament-match-right-score">-</h2>
-  //         <div class="tournament-match-player-info-right">
-  //           <img loading="lazy" src="${
-  //             player2.avatar || "./assets/default_avatar.png"
-  //           }" alt="Player avatar" class="tournament-match-avatar" />
-  //           <h3 class="game-player-name tournament">${player2.label}</h3>
-  //         </div>
-  //       </div>
-  //     `;
-  // }
+  function arraysChecker(arr1, arr2) {
+    const set1 = new Set(arr1);
+    return arr2.every((element) => set1.has(element));
+  }
 
-  // function generateTitle(title) {
-  //   return `<h1 class="tournament-name-text">${title}</h1>`;
-  // }
+  function generatePlayerCards(tournamentPlayers) {
+    tournamentGrid.innerHTML = "";
 
-  // function generateSemifinals(filledPlayers) {
-  //   shuffle(filledPlayers);
-  //   const [player1, player2, player3, player4] = filledPlayers;
-    
-  //   window.tournamentState.userIds = [];
-  //   window.tournamentState.userIds.push(
-  //     player1.id,
-  //     player2.id,
-  //     player3.id,
-  //     player4.id
-  //   );
-    
-  //   firstSemifinalContent.innerHTML = "";
-  //   secondSemifinalContent.innerHTML = "";
-    
-  //   firstSemifinalContent.innerHTML = `
-  //   <h1 class="tournament-match-title">Semifinal 1</h1>
-  //   ${generateMatchContent(player1, player2)}
-  //   `;
-    
-  //   secondSemifinalContent.innerHTML = `
-  //   <h1 class="tournament-match-title" >Semifinal 2</h1>
-  //   ${generateMatchContent(player3, player4)}
-  //   `;
-  // }
+    const filledPlayers = [...tournamentPlayers];
 
-  // const players = getPlayers();
+    filledPlayers.forEach((player, index) => {
+      const playerCard = document.createElement("div");
+      playerCard.className = "game-player-card";
+      playerCard.innerHTML = `
+          <article class="tournament-game-player-card-inner">
+            <h2 class="game-player-number">Player ${index + 1}</h2>
+            <img
+              loading="lazy"
+              src="${player.avatar || "./assets/default_avatar.png"}"
+              alt="Player avatar"
+              class="game-player-avatar"
+            />
+            <h3 class="game-player-name">${player.username}</h3>
+            <div class="game-player-win-rate-container">
+              <p class="game-player-statistics-param">Win rate</p>
+              <p class="game-player-statistics-param-number last">${
+                player.winRate ?? 0
+              }</p>
+            </div>
+          </article>
+        `;
+      tournamentGrid.appendChild(playerCard);
+    });
+    return filledPlayers;
+  }
 
-  // if (players.length > 0) {
-  //   const filledPlayers = generatePlayerCards(players);
-  //   addLabelsToPlayers(filledPlayers);
+  function addLabelsToPlayers(players) {
+    players.forEach((player, index) => {
+      player.label = `Player ${index + 1}`;
+    });
+  }
 
-  //   const tournamentName = getTournamentName();
+  function generateMatchContent(player1, player2) {
+    return `
+        <div class="tournament-match-players">
+          <div class="tournament-match-player-info-left">
+            <img loading="lazy" src="${
+              player1?.avatar || "./assets/default_avatar.png"
+            }" alt="Player avatar" class="tournament-match-avatar" />
+            <h3 class="game-player-name tournament">${player1?.label}</h3>
+          </div>
+          <h2 class="tournament-match-left-score">-</h2>
+          <img class="tournament-match-vs-logo" src="./assets/vs_logo.png" alt="Versus logo" />
+          <h2 class="tournament-match-right-score">-</h2>
+          <div class="tournament-match-player-info-right">
+            <img loading="lazy" src="${
+              player2?.avatar || "./assets/default_avatar.png"
+            }" alt="Player avatar" class="tournament-match-avatar" />
+            <h3 class="game-player-name tournament">${player2?.label}</h3>
+          </div>
+        </div>
+      `;
+  }
 
-  //   window.tournamentState.name = tournamentName;
-  //   tournamentTitle.innerHTML = generateTitle(window.tournamentState.name);
+  function generateTitle(title) {
+    return `<h1 class="tournament-name-text">${title}</h1>`;
+  }
 
-  //   generateSemifinals(filledPlayers);
-  // }
+  function generateSemifinals(filledPlayers) {
+    const [player1, player2, player3, player4] = filledPlayers;
 
-  // // Reset tournament state on "Back to menu"
-  // function resetTournament() {
-  //   window.tournamentState.name = "";
+    window.tournamentState.userIds = [];
+    window.tournamentState.userIds.push(
+      player1?.id,
+      player2?.id,
+      player3?.id,
+      player4?.id
+    );
 
-  //   firstSemifinalContent.innerHTML =
-  //     '<h2 class="tournament-round h2" id="firstSemifinal">Semifinal 1</h2>';
-  //   secondSemifinalContent.innerHTML =
-  //     '<h2 class="tournament-round h2" id="secondSemifinal">Semifinal 2</h2>';
-  // }
+    firstSemifinalContent.innerHTML = "";
+    secondSemifinalContent.innerHTML = "";
 
-  // const backToMenuButton = document.getElementById("backToMenu");
-  // backToMenuButton.addEventListener("click", function () {
-  //   resetTournament();
-  //   goToLobby();
-  // });
+    firstSemifinalContent.innerHTML = `
+    <h1 class="tournament-match-title">Semifinal 1</h1>
+    ${generateMatchContent(player1, player2)}
+    `;
+
+    secondSemifinalContent.innerHTML = `
+    <h1 class="tournament-match-title" >Semifinal 2</h1>
+    ${generateMatchContent(player3, player4)}
+    `;
+  }
+
+  const filledPlayers = generatePlayerCards(tournamentPlayers);
+  addLabelsToPlayers(filledPlayers);
+  tournamentTitle.innerHTML = generateTitle(tournament?.name);
+
+  generateSemifinals(filledPlayers);
+
+  function resetTournament() {
+    window.tournamentState.name = "";
+
+    firstSemifinalContent.innerHTML =
+      '<h2 class="tournament-round h2" id="firstSemifinal">Semifinal 1</h2>';
+    secondSemifinalContent.innerHTML =
+      '<h2 class="tournament-round h2" id="secondSemifinal">Semifinal 2</h2>';
+  }
+
+  const backToMenuButton = document.getElementById("backToMenu");
+  backToMenuButton.addEventListener("click", function () {
+    resetTournament();
+    goToLobby();
+  });
 }
