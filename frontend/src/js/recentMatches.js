b05026179160174760ca5a1bcb15e0e1174b26a4
MCP <91376232+madfcat@users.noreply.github.com>
2024-12-10 18:41:54 +0200

    Add solution for match statistics

--- frontend/src/js/recentMatches.js
+++ frontend/src/js/recentMatches.js
@@ -10,10 +10,12 @@ async function renderRecentMatches() {
 	data?.matches?.map((match) => {
 		const newDiv = document.createElement("div");
 		let isWinner1 = undefined;
-		if (match.score !== null) {	
+		if (match.score !== null) {
 			isWinner1 = Number(match.score[0]) > Number(match.score[1]);
 		}
-		const matchScore = match.score ? match.score[0] + ":" + match.score[1] : "0:0";
+		const matchScore = match.score
+			? match.score[0] + ":" + match.score[1]
+			: "0:0";
 
 		let team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
 							${match.players[0]?.displayName ?? match.players[0]?.username}
@@ -22,7 +24,6 @@ async function renderRecentMatches() {
 							${match.players[1]?.displayName ?? match.players[1]?.username}
 						</div>`;
 		if (match.players.length == 4) {
-			// console.log(match.score[0], match.score[1], isWinner1);
 			team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
 							<div class="player-small-text">
 							${match.players[0]?.displayName ?? match.players[0]?.username}
@@ -45,10 +46,9 @@ async function renderRecentMatches() {
 					${match.score ? "[" + matchScore + "]" : "[0:0]"}
 				</div>`;
 
-
 		newDiv.innerHTML = `
 			<li class="match-result">
-			<div onclick="goToMatchView()" class="match-link">
+			<div onclick="goToMatchView(${match.id})" class="match-link">
 				${team1Div}
 				<div class="versus-text">vs</div>
 				${team2Div}
@@ -76,9 +76,164 @@ async function fetchRecentMatches() {
 			throw new Error("No data");
 		}
 		return data;
-
 	} catch (error) {
 		console.error(error);
 		return null;
+	}
+}
+
+// async function renderH2h(player1Id, player2Id) {
+// 	try {
+// 		const response = await fetch(`/api/players/${player1Id}/matches/`, {
+// 			method: "GET",
+// 			headers: {
+// 				"Content-Type": "application/json",
+// 			},
+// 		});
+// 		if (!response.ok) {
+// 			console.error(
+// 				`HTTP error getting the match stats! status: ${response.status}`
+// 			);
+// 			return null;
+// 		}
+// 		const { data } = await response.json();
+// 		const matches = data.matches?.filter((match) => {
+// 			return match.score && match.players?.length === 2;
+// 		});
+// 		console.log("matches: ", matches);
+
+// 		return `
+// 			<div class="match-stats-header-no-arrow">
+// 				<h1 class="match-stats-title">H2H</h1>
+// 			</div>
+// 			<div class="match-stats-result h2h-stats-result">
+// 				<div class="match-stats-text-format">
+// 					<span>Vladimir</span>
+// 					<span class="match-stats-match-score">7:4</span>
+// 					<span>Max</span>
+// 				</div>
+// 			</div>
+// 			<div class="match-stats-header-no-arrow">
+// 				<h1 class="match-stats-title">H2H matches</h1>
+// 			</div>
+// 			<div class="match-stats-scroll-container">
+// 				<ul class="match-stats-list h2h-matches">
+// 					<li class="match-stats-text-format">
+// 						<span>Vladimir</span>
+// 						<span class="match-stats-match-score">11:2</span>
+// 						<span>Max</span>
+// 					</li>
+// 					<li class="match-stats-text-format">
+// 						<span>Max</span>
+// 						<span class="match-stats-match-score">9:11</span>
+// 						<span>Vladimir</span>
+// 					</li>
+// 					<li class="match-stats-text-format">
+// 						<span>Vladimir</span>
+// 						<span class="match-stats-match-score">4:11</span>
+// 						<span>Max</span>
+// 					</li>
+// 					<li class="match-stats-text-format">
+// 						<span>Vladimir</span>
+// 						<span class="match-stats-match-score">13:11</span>
+// 						<span>Max</span>
+// 					</li>
+// 					<li class="match-stats-text-format">
+// 						<span>Max</span>
+// 						<span class="match-stats-match-score">10:12</span>
+// 						<span>Vladimir</span>
+// 					</li>
+// 				</ul>
+// 			</div>
+// 			`;
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// }
+
+async function renderDetailedMatchStats(matchId) {
+	function secondsToHms(d) {
+		d = Number(d);
+		const h = Math.floor(d / 3600);
+		const m = Math.floor((d % 3600) / 60);
+		const s = Math.floor((d % 3600) % 60);
+
+		const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+		const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+		const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+		return hDisplay + mDisplay + sDisplay;
+	}
+
+	function isoDateToShortDate(isoDate) {
+		const date = new Date(isoDate);
+		return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+	}
+
+	try {
+		const renderElement = document.querySelector(".matchView .match-stats");
+		const response = await fetch(`/api/matches/${matchId}/`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		if (!response.ok) {
+			console.error(
+				`HTTP error getting the match stats! status: ${response.status}`
+			);
+			return null;
+		}
+		const { data } = await response.json();
+		console.log(data);
+
+/*		const player1Id = data.players[0].id;
+		const player2Id = data.players[1].id;
+
+		const h2hRender = await renderH2h(player1Id, player2Id);*/
+
+		const team1Div =	data.players.length === 2 ? 
+							`<div>${data.players[0].displayName || data.players[0].username}</div>` :
+							`<div class="player-small-text">${data.players[0].displayName || data.players[0].username}</div>
+							<div class="player-small-text">${data.players[1].displayName || data.players[1].username}</div>`;
+		const team2Div =	data.players.length === 2 ? 
+							`<div>${data.players[1].displayName || data.players[1].username}</div>` :
+							`<div class="player-small-text">${data.players[2].displayName || data.players[2].username}</div>
+							<div class="player-small-text">${data.players[3].displayName || data.players[3].username}</div>`;
+
+		renderElement.innerHTML = `
+			<div class="match-stats-header">
+				<header class="match-stats-header-wrapper">
+					<p onclick="goToHomeRight()" class="animation">
+						<img src="./assets/back_arrow.svg" alt="" />
+					</p>
+					<h1 class="match-stats-title-top">Match statistics</h1>
+				</header>
+				</div>
+				<div class="match-stats-result">
+					<div class="match-stats-text-format">
+						<div>${team1Div}</div>
+						<div>${data.score[0]}</div>
+
+					</div>
+					<div class="match-stats-text-format">
+						<div>${team2Div}</div>	
+						<div>${data.score[1]}</div>
+					</div>
+				</div>
+				<div class="match-stats-result match-stats-time-info">
+					<div class="match-stats-text-format">
+						<span>Match time:</span>
+						<span>${secondsToHms(data.duration)}</span>
+					</div>
+					<div class="match-stats-text-format">
+						<span>Date:</span>
+						<span>${isoDateToShortDate(data.createdAt)}</span>
+					</div>
+				</div>
+
+			</div>
+			`;
+	} catch (error) {
+		console.error(error);
 	}
 }
