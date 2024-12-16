32cfefa2436f897244fd85d0e555c538cb286d7d
MCP <91376232+madfcat@users.noreply.github.com>
2024-12-17 01:36:02 +0200

    Remove AI from friends list, Sort players (requests, friends, others)

--- frontend/src/js/profileSettings.js
+++ frontend/src/js/profileSettings.js
@@ -1,198 +1,204 @@
 const updateToProfile = async (index) => {
-  const userId = window.state["loggedInUsers"][index].id;
-  window.currentUserID = index;
-  try {
-    const response = await fetch(`/api/players/${userId}/`, {
-      method: "GET",
-    });
+	const userId = window.state["loggedInUsers"][index].id;
+	window.currentUserID = index;
+	try {
+		const response = await fetch(`/api/players/${userId}/`, {
+			method: "GET",
+		});
 
-    if (!response.ok) {
-      throw new Error("Failed to get user info");
-    }
-    const json = await response.json();
-    console.log(json.data.avatarUrl);
-    document.getElementById("person-avatar").src =
-      json.data.avatarUrl || "./assets/default_avatar.png";
-    document.getElementById("person-name").innerText = json.data.displayName;
-  } catch (error) {
-    console.error(error.message);
-  }
+		if (!response.ok) {
+			throw new Error("Failed to get user info");
+		}
+		const json = await response.json();
+		document.getElementById("person-avatar").src =
+			json.data.avatarUrl || "./assets/default_avatar.png";
+		document.getElementById("person-name").innerText = json.data.displayName;
+	} catch (error) {
+		console.error(error.message);
+	}
 
-  try {
-    const response = await fetch(`/api/players/${userId}/stats`, {
-      method: "GET",
-    });
+	try {
+		const response = await fetch(`/api/players/${userId}/stats`, {
+			method: "GET",
+		});
 
-    if (!response.ok) {
-      throw new Error("Failed to get user info");
-    }
-    const {data } = await response.json();
-	const winsLoses = `	<div>Wins: </div>
-						<div>
-						${data.wins || 0}
+		if (!response.ok) {
+			throw new Error("Failed to get user info");
+		}
+		const { data } = await response.json();
+		const winsLoses = `
+							<div>Wins: </div><div>${data.wins || 0}</div>
+							<div>Loses: </div><div>${data.loses || 0}</div>
+							`;
+		const element = document.querySelector(".player-wins-loses");
+		element.innerHTML = winsLoses;
+	} catch (error) {
+		console.error(error.message);
+	}
+
+	try {
+		const response = await fetch(`/api/players/${userId}/matches?last=5`, {
+			method: "GET",
+		});
+
+		if (!response.ok) {
+			goToLobby();
+			throw new Error("Failed to get user matches");
+		}
+		const json = await response.json();
+		const matches = document.getElementById("person-stats");
+		if (json.data.matches?.length == 0) matches.innerText = "No Matches yet";
+		else {
+			matches.innerHTML = "";
+			json.data.matches?.forEach((match) => {
+				const newDiv = document.createElement("ul");
+				newDiv.classList.add("match-results");
+				let isWinner1 = undefined;
+				if (match.score !== null) {
+					isWinner1 = Number(match.score[0]) > Number(match.score[1]);
+				}
+				const matchScore = match.score
+					? match.score[0] + ":" + match.score[1]
+					: "0:0";
+
+				let team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
+								${match.players[0]?.displayName ?? match.players[0]?.username}
+								</div>`;
+				let team2Div = `<div class=${isWinner1 === false ? "winner-name" : ""}>
+									${match.players[1]?.displayName ?? match.players[1]?.username}
+								</div>`;
+				if (match.players.length == 4) {
+					team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
+									<div class="player-small-text">
+									${match.players[0]?.displayName ?? match.players[0]?.username}
+									</div>
+									<div class="player-small-text">
+									${match.players[1]?.displayName ?? match.players[1]?.username}
+									</div>
+								</div>`;
+					team2Div = `<div class=${isWinner1 === false ? "winner-name" : ""}>
+									<div class="player-small-text">
+									${match.players[2]?.displayName ?? match.players[2]?.username}
+									</div>
+									<div class="player-small-text">
+									${match.players[3]?.displayName ?? match.players[3]?.username}
+									</div>
+								</div>`;
+				}
+
+				const matchScoreDiv = `<div>
+					  ${match.score ? "[" + matchScore + "]" : "[0:0]"}
+				  </div>`;
+
+				newDiv.innerHTML = `
+								<li class="match-result">
+									<div class="match-link no-cursor no-hover">
+										<div>${isoDateToShortDate(match.createdAt)}</div>
+										${team1Div}
+										<div class="versus-text">vs</div>
+										${team2Div}
+										${matchScoreDiv}
+									</div>
+								</li>`;
+				matches.append(newDiv);
+			});
+		}
+	} catch (error) {
+		console.error(error.message);
+	}
+
+	try {
+		const response = await fetch(`/api/players/`, {
+			method: "GET",
+		});
+
+		if (!response.ok) {
+			goToLobby();
+			throw new Error("Failed to get potential friends");
+		}
+
+		const friendsRequest = await fetch(`/api/players/${userId}/friends/`, {
+			method: "GET",
+		});
+
+		if (!response.ok) {
+			goToLobby();
+			throw new Error("Failed to get actual friends");
+		}
+
+		// Friends
+		const friends = await friendsRequest.json();
+
+		const json = await response.json();
+		const players = document.getElementById("friends");
+		if (json.data.players.length == 0) matches.innerText = "No one else exists";
+		else {
+			let requestPlayers = "";
+			let friendPlayers = "";
+			let otherPlayers = "";
+			const playersArr = json.data.players.filter(
+				(player) => player.username != "ai_player"
+			);
+			playersArr.forEach((player) => {
+				let friendMarker = friends.data.friends.find(
+					(friend) => friend.id == player.id
+				);
+				let status = "";
+
+				if (friendMarker) {
+					if (friendMarker.complete) status = friendMarker.status;
+					else if (friendMarker.forMe)
+						status = `
+								<button onclick='approveFriendship(${player.id})'>Approve</button>
+								<button onclick='denyFriendship(${player.id})'>Deny</button>
+						`;
+					else if (friendMarker.forOther)
+						status = "Waiting on approval of friendship";
+				} else {
+					status =
+						"<button onclick='requestFriendship(" +
+						player.id +
+						")'>Request Friendship</button>";
+				}
+
+				let content = `
+				<div class="carousel-item">
+					<img class="friend-avatar" src="${
+						player.avatarUrl || "./assets/default_avatar.png"
+					}">
+					<h1 class="friend-name">${player.displayName || player.username}</h1>
+					<div style="display: flex; flex-direction: row; justify-content: center; align-items: center; gap: 0.5vw;">
+						${
+							friendMarker && friendMarker.complete
+								? `<img class="friend-login-status" src="./assets/${
+										friendMarker.status === "Online"
+											? "online.png"
+											: "offline.png"
+								  }">`
+								: ""
+						}
+						<div class="friend-status">
+						${status}
 						</div>
-						<div>Loses: </div>
-						<div>
-						${data.loses || 0}
-						</div>`
-	const element = document.querySelector(".player-wins-loses");
-	element.innerHTML = winsLoses;
-  } catch (error) {
-	console.error(error.message);
-  }
+					</div>
+				</div>
+				`;
+				if (friendMarker?.forMe) requestPlayers += content;
+				else if (friendMarker?.complete || friendMarker?.forOther)
+					friendPlayers += content;
+				else otherPlayers += content;
+			});
 
-  try {
-    const response = await fetch(`/api/players/${userId}/matches?last=5`, {
-      method: "GET",
-    });
-
-    if (!response.ok) {
-      goToLobby();
-      throw new Error("Failed to get user matches");
-    }
-    const json = await response.json();
-    const matches = document.getElementById("person-stats");
-    if (json.data.matches?.length == 0) matches.innerText = "No Matches yet";
-    else {
-      matches.innerHTML = "";
-      json.data.matches?.forEach((match) => {
-		const newDiv = document.createElement("ul");
-		newDiv.classList.add("match-results");
-		let isWinner1 = undefined;
-		if (match.score !== null) {
-			isWinner1 = Number(match.score[0]) > Number(match.score[1]);
+			players.innerHTML = (
+				requestPlayers +
+				friendPlayers +
+				otherPlayers
+			).replace("carousel-item", "carousel-item active");
 		}
-		const matchScore = match.score
-			? match.score[0] + ":" + match.score[1]
-			: "0:0";
-
-		let team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
-							${match.players[0]?.displayName ?? match.players[0]?.username}
-						</div>`;
-		let team2Div = `<div class=${isWinner1 === false ? "winner-name" : ""}>
-							${match.players[1]?.displayName ?? match.players[1]?.username}
-						</div>`;
-		if (match.players.length == 4) {
-			team1Div = `<div class=${isWinner1 === true ? "winner-name" : ""}>
-							<div class="player-small-text">
-							${match.players[0]?.displayName ?? match.players[0]?.username}
-							</div>
-							<div class="player-small-text">
-							${match.players[1]?.displayName ?? match.players[1]?.username}
-							</div>
-						</div>`;
-			team2Div = `<div class=${isWinner1 === false ? "winner-name" : ""}>
-							<div class="player-small-text">
-							${match.players[2]?.displayName ?? match.players[2]?.username}
-							</div>
-							<div class="player-small-text">
-							${match.players[3]?.displayName ?? match.players[3]?.username}
-							</div>
-						</div>`;
-		}
-
-		const matchScoreDiv = `<div>
-					${match.score ? "[" + matchScore + "]" : "[0:0]"}
-				</div>`;
-
-		newDiv.innerHTML = `
-			<li class="match-result">
-			<div class="match-link no-cursor no-hover">
-				<div>${isoDateToShortDate(match.createdAt)}</div>
-				${team1Div}
-				<div class="versus-text">vs</div>
-				${team2Div}
-				${matchScoreDiv}
-			</div>
-			</li>`;
-        matches.append(newDiv);
-      });
-    }
-  } catch (error) {
-    console.error(error.message);
-  }
-
-  try {
-    const response = await fetch(`/api/players/`, {
-      method: "GET",
-    });
-
-    if (!response.ok) {
-      goToLobby();
-      throw new Error("Failed to get potential friends");
-    }
-
-    const friendsRequest = await fetch(`/api/players/${userId}/friends/`, {
-      method: "GET",
-    });
-
-    if (!response.ok) {
-      goToLobby();
-      throw new Error("Failed to get actual friends");
-    }
-
-    const friends = await friendsRequest.json();
-
-    const json = await response.json();
-    const players = document.getElementById("friends");
-    if (json.data.players.length == 0) matches.innerText = "No one else exists";
-    else {
-      players.innerHTML = "";
-      json.data.players.forEach((player, index) => {
-        let friend_marker = friends.data.friends.find(
-          (friend) => friend.id == player.id
-        );
-        let status = "";
-
-        if (friend_marker) {
-          if (friend_marker.complete) status = friend_marker.status;
-          else if (friend_marker.forMe)
-            status = `
-                <button onclick='approveFriendship(${player.id})'>Approve</button>
-                <button onclick='denyFriendship(${player.id})'>Deny</button>
-              `;
-          else if (friend_marker.forOther)
-            status = "Waiting on approval of friendship";
-        } else {
-          status =
-            "<button onclick='requestFriendship(" +
-            player.id +
-            ")'>Request Friendship</button>";
-        }
-
-        console.log(player);
-
-        let content = `
-          <div class="carousel-item ${index == 0 ? "active" : ""}">
-            <img class="friend-avatar" src="${
-              player.avatarUrl || "./assets/default_avatar.png"
-            }">
-            <h1 class="friend-name">${
-              player.displayName || player.username
-            }</h1>
-            <div style="display: flex; flex-direction: row; justify-content: center; align-items: center; gap: 0.5vw;">
-              ${
-                friend_marker && friend_marker.complete
-                  ? `<img class="friend-login-status" src="./assets/${
-                      friend_marker.status === "Online"
-                        ? "online.png"
-                        : "offline.png"
-                    }">`
-                  : ""
-              }
-              <div class="friend-status">
-                ${status}
-              </div>
-            </div>
-          </div>
-          `;
-        if (player.id != userId) players.innerHTML += content;
-      });
-    }
-  } catch (error) {
-    console.error(error.message);
-  }
-  goToProfile();
+	} catch (error) {
+		console.error(error.message);
+	}
+	goToProfile();
 	nameUpdate(userId);
 };
 
@@ -215,7 +221,6 @@ document.getElementById("newAvatar").onchange = async (e) => {
 		return;
 	}
 	alert("Succesful file upload");
-	console.log(window.currentUserID);
 	await updateToProfile(window.currentUserID);
 };
 
@@ -228,7 +233,10 @@ function nameUpdate(userId) {
 	buttonElement.addEventListener("click", handleClick);
 
 	const inputHandler = () => {
-		nameElement.textContent = nameElement.textContent.replace(/[^a-zA-Z0-9\s]/g, "");
+		nameElement.textContent = nameElement.textContent.replace(
+			/[^a-zA-Z0-9\s]/g,
+			""
+		);
 		// Ensure the name element has a text node
 		if (!nameElement.childNodes[0]) {
 			nameElement.appendChild(document.createTextNode(""));
@@ -237,22 +245,23 @@ function nameUpdate(userId) {
 		const updatedRange = document.createRange();
 		const selection = window.getSelection();
 
-		updatedRange.setStart(nameElement.childNodes[0], nameElement.textContent.length || 0);
+		updatedRange.setStart(
+			nameElement.childNodes[0],
+			nameElement.textContent.length || 0
+		);
 		updatedRange.collapse(true);
-		
+
 		selection.removeAllRanges();
 		selection.addRange(updatedRange);
 	};
 
 	const handleEnter = async (event) => {
-		if (event.key === 'Enter') {
-			await saveName();	
+		if (event.key === "Enter") {
+			await saveName();
 		}
 	};
-	
+
 	const saveName = async () => {
-		console.log("saving...", nameElement);
-		console.log("saving...", backupName);
 		try {
 			const response = await fetch(`/api/players/${userId}/`, {
 				method: "PATCH",
@@ -260,16 +269,14 @@ function nameUpdate(userId) {
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify({ displayName: nameElement.textContent }),
-			})
+			});
 			if (!response.ok) {
 				throw new Error("Failed to update name");
 			}
-		}
-		catch (error) {
+		} catch (error) {
 			console.error(error.message);
 			nameElement.textContent = backupName;
-		}
-		finally {
+		} finally {
 			nameElement.setAttribute("contenteditable", false);
 			backupName = nameElement.innerText;
 		}
@@ -281,7 +288,7 @@ function nameUpdate(userId) {
 		nameElement.blur();
 		nameElement.setAttribute("contenteditable", false);
 	};
-	
+
 	function handleClick() {
 		if (!nameElement.childNodes[0]) {
 			nameElement.appendChild(document.createTextNode(""));
@@ -289,21 +296,23 @@ function nameUpdate(userId) {
 		const range = document.createRange();
 		const selection = window.getSelection();
 		// Place the cursor at the end of the content
-		range.setStart(nameElement.childNodes[0], nameElement.textContent.length || 0);
+		range.setStart(
+			nameElement.childNodes[0],
+			nameElement.textContent.length || 0
+		);
 		range.collapse(true);
-		
+
 		// Clear any existing selections and set the new range
 		selection.removeAllRanges();
 		selection.addRange(range);
 
 		nameElement.setAttribute("contenteditable", true);
 		nameElement.focus();
-		
+
 		// Remove any existing input listener and re-add it
 		nameElement.removeEventListener("input", inputHandler); // Remove previous listener (if exists)
 		nameElement.addEventListener("input", inputHandler);
-		
-		
+
 		nameElement.removeEventListener("blur", saveName);
 		nameElement.addEventListener("blur", saveName);
 
